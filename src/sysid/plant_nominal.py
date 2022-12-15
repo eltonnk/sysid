31c62ef62b68ae1efac9173a129a70cae0e7c5d3
eltonnk <ahenri333@gmail.com>
2022-12-15 14:18:12 -0500

    Added a bit more bode plot versatility

--- src/sysid/plant_nominal.py
+++ src/sysid/plant_nominal.py
@@ -2,6 +2,8 @@ import control
 import numpy as np
 from typing import List, Callable
 from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 from sklearn.cluster import MeanShift
 
@@ -119,22 +121,26 @@ def find_nom_plant_with_clust(
     den = np.poly(np.ravel(poles_cluster_centers))
     return control.tf(num, den)
 
-def plot_nom_vs_all(P_nom, plant_list):
-    # Plot nominal plant vs all other plants
+def setupt_magnitude_plots() ->  tuple[Figure, list[Axes], np.ndaray]:
     w_shared = np.arange(0.01, 1000.0, 0.01)
-    list_mag_abs = np.vstack([control.bode(plant, w_shared, plot=False)[0] for plant in plant_list])
-    list_mag_dB = 20 * np.log10(list_mag_abs)
-    mag_abs_nom = control.bode(P_nom, w_shared, plot=False)[0]
-    mag_dB_nom = 20 * np.log10(mag_abs_nom)
-
     fig, ax = plt.subplots(2, 1)
     ax[0].set_ylabel(r'$|R_k(j\omega)|$ (dB)')
     ax[1].set_ylabel(r'$||R_k(j\omega)|$ (absolute)')
+
+    return fig, ax, w_shared
+
+def add_list_plant_to_mag_plot(
+    w_shared:np.ndarray,
+    ax: list[Axes], 
+    plant_list: list[control.TransferFunction],
+):
+    list_mag_abs = np.vstack([control.bode(plant, w_shared, plot=False)[0] for plant in plant_list])
+    list_mag_dB = 20 * np.log10(list_mag_abs)
     for mag_dB, mag_abs, index in zip(list_mag_dB, list_mag_abs, range(len(list_mag_dB))):
         ax[0].plot(w_shared, mag_dB, label=f'P{index}', color=f'C{index}')
         ax[1].plot(w_shared, mag_abs, label=f'P{index}', color=f'C{index}')
-    ax[0].plot(w_shared, mag_dB_nom, label=f'P_nom', color=f'black')
-    ax[1].plot(w_shared, mag_abs_nom, label=f'P_nom', color=f'black')
+
+def finish_magnitude_plots(fig:Figure, ax:Axes):
     for a in np.ravel(ax):
         a.set_xlabel(r'$\omega$ (rad/s)')
         a.set_xscale('log')
@@ -142,3 +148,14 @@ def plot_nom_vs_all(P_nom, plant_list):
     ax[0].legend(loc='lower left', ncol=3)
     ax[1].legend(loc='upper right', ncol=3)
     fig.tight_layout()
+
+def plot_nom_vs_all(P_nom: control.TransferFunction, plant_list: list[control.TransferFunction]):
+    # Plot nominal plant vs all other plants
+    fig, ax, w_shared = setupt_magnitude_plots()
+    add_list_plant_to_mag_plot(w_shared, ax, plant_list)
+    # Add nominal plant bode plot over bode plant of all plants
+    mag_abs_nom = control.bode(P_nom, w_shared, plot=False)[0]
+    mag_dB_nom = 20 * np.log10(mag_abs_nom)
+    ax[0].plot(w_shared, mag_dB_nom, label=f'P_nom', color=f'black')
+    ax[1].plot(w_shared, mag_abs_nom, label=f'P_nom', color=f'black')
+    finish_magnitude_plots(fig, ax)
